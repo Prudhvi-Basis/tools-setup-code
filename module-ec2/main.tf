a8ceb53ef46c14052fb6ae2773d140bffaf0e784
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-08 18:13:57 +0530

    Terraform

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -1,38 +1,38 @@
-#
-# resource "aws_security_group" "sg" {
-#   name        = "${var.tool_name}-sg"
-#   description = "Inbound allow for ${var.tool_name}"
-#
-#   ingress {
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "TCP"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#
-#   }
-#   ingress {
-#     from_port        = var.sg_port
-#     to_port          = var.sg_port
-#     protocol         = "TCP"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#
-#   }
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#
-#   }
-#   tags = {
-#     Name = "${var.tool_name}-sg"
-#   }
-# }
+
+resource "aws_security_group" "sg" {
+  name        = "${var.tool_name}-sg"
+  description = "Inbound allow for ${var.tool_name}"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    from_port        = var.sg_port
+    to_port          = var.sg_port
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  tags = {
+    Name = "${var.tool_name}-sg"
+  }
+}
 
 resource "aws_instance" "instance" {
-   ami           = data.aws_ami.ami.id
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  # vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = var.tool_name
 
