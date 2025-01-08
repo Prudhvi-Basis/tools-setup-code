33576f2221e8f2658624c61054b0a3e71795aa9f
prudhi4321 <prudhviraj3525@gmail.com>
2025-01-08 20:14:19 +0530

    Terraform

--- misc/vault_secrets/main.tf
+++ misc/vault_secrets/main.tf
@@ -37,4 +37,116 @@ resource "vault_generic_secret" "roboshop-dev" {
 EOT
 }
 
+resource "vault_generic_secret" "catalogue-dev" {
 
+  path                 =  "${vault_mount.roboshop-dev.path}/catalogue"
+
+
+  data_json = <<EOT
+  {
+   "MONGO" : "true"
+   "MONGO_URL" : "mongodb://mongodb-dev.devops11.online:27017/catalogue"
+   "CATALOGUE_PORT" : "8080"
+
+
+}
+EOT
+}
+resource "vault_generic_secret" "user-dev" {
+
+path                 =  "${vault_mount.roboshop-dev.path}/user"
+
+
+data_json = <<EOT
+  {
+   "MONGO" : "true"
+   "MONGO_URL" : "mongodb://mongodb-dev.devops11.online:27017/user",
+   "REDIS_URL" : "redis://redis-dev.devops11.online:6379"
+
+
+}
+EOT
+}
+
+
+resource "vault_generic_secret" "cart-dev" {
+
+  path                 =  "${vault_mount.roboshop-dev.path}/cart"
+
+
+  data_json = <<EOT
+  {
+
+   "REDIS_HOST" : "redis-dev.devops11.online",
+   "CATALOGUE_HOST" : "catalogue-dev.devops11.online"
+
+
+}
+EOT
+}
+
+resource "vault_generic_secret" "shipping-dev" {
+
+  path                 =  "${vault_mount.roboshop-dev.path}/shipping"
+
+
+  data_json = <<EOT
+  {
+   "mysql_root_password" : "Roboshop@1"
+   "CART_ENDPOINT" : "cart-dev.devops11.online:8080",
+   "DB_HOST" : "mysql-dev.devops11.online",
+
+
+
+}
+EOT
+}
+
+resource "vault_generic_secret" "payment-dev" {
+
+  path                 =  "${vault_mount.roboshop-dev.path}/payment"
+
+
+  data_json = <<EOT
+  {
+   "CART_HOST" : "cart-dev.devops11.online"
+   "USER_HOST" : "user-dev.devops11.online",
+   "AMQP_HOST" : "rabbitmq-dev.devops11.online",
+   "CART_PORT" : "8080",
+   "USER_PORT" : "8080",
+   "AMQP_USER" : "roboshop"
+   "AMQP_PASS" : "roboshop123"
+
+
+
+}
+EOT
+}
+resource "vault_generic_secret" "mysql-dev" {
+
+  path                 =  "${vault_mount.roboshop-dev.path}/mysql"
+
+
+  data_json = <<EOT
+  {
+   "mysql_root_password" : "Roboshop@1"
+
+
+
+}
+EOT
+}
+resource "vault_generic_secret" "rabbitmq-dev" {
+
+  path                 =  "${vault_mount.roboshop-dev.path}/rabbitmq"
+
+
+  data_json = <<EOT
+  {
+   "user" : "roboshop"
+   "password" : "Roboshop@123",
+
+
+}
+EOT
+}
